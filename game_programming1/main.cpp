07572dc3d05c443f6c26f1002b785bef6201d858
Brian Jones <brianmj@brians-mbp.fios-router.home>
2019-01-21 18:33:42 -0500

    Initial Commit

---
+++ game_programming1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game_programming1
+//
+//  Created by Brian Jones on 1/21/19.
+//  Copyright © 2019 Brian Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
